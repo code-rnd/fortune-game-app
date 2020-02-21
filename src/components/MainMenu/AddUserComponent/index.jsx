4c935107b7860d4725f3076ba85b1102cd700883
Philip Petrov <codernd.blog@gmail.com>
2020-02-21 12:49:26 +0500

    add gitignore

--- src/components/MainMenu/AddUserComponent/index.jsx
+++ src/components/MainMenu/AddUserComponent/index.jsx
@@ -1,26 +1,33 @@
-import React, {useState} from "react";
-
-import './style.scss';
+import React, { useState } from "react";
 
 export default function AddUserComponent(props) {
+  const { addUser } = props;
 
-    const {addUser} = props;
+  const [isNameUser, setNameUser] = useState("");
 
-    const [isNameUser, setNameUser] = useState('');
+  const handleAddUser = () => {
+    addUser(isNameUser);
+    setNameUser("");
+  };
 
-    const handleAddUser = () => {
-        addUser(isNameUser);
-        setNameUser('');
-    }
-
-    return (
-        <div className={'addUser user users-addUser margin-top20px'}>
-            <input type={'text'} placeholder={'new user..'} value={isNameUser} onChange={(e) => {
-                setNameUser(e.currentTarget.value)
-            }}/>
-            <input type={'button'} value={'add user'} onClick={() => {
-                handleAddUser()
-            }} disabled={!isNameUser || isNameUser === ' '}/>
-        </div>
-    )
+  return (
+    <div className={"addUser user users-addUser margin-top20px"}>
+      <input
+        type={"text"}
+        placeholder={"new user.."}
+        value={isNameUser}
+        onChange={e => {
+          setNameUser(e.currentTarget.value);
+        }}
+      />
+      <input
+        type={"button"}
+        value={"add user"}
+        onClick={() => {
+          handleAddUser();
+        }}
+        disabled={!isNameUser || isNameUser === " "}
+      />
+    </div>
+  );
 }
